111fcb52c0a7449cdd651f72bcca0306c5b424a8
dewskii <dewskii@This-Computer-Right-Here.local>
2016-02-11 12:05:41 -0500

    added 3 grids to the main class to resemble the puzzle, answers are set as the same array for testing purposes

--- Main.java
+++ Main.java
@@ -16,10 +16,20 @@ public class Main extends Application {
 	  @Override
 	    public void start(Stage primaryStage) {
 	        primaryStage.setTitle("GridPane example");
-	        Block blocktest = new Block(testAnswers);
-		blocktest.setButtons();
-	        GridPane grid1 = blocktest.returnGrid();
-	        Scene scene = new Scene(grid1);
+	        Block blocktest1 = new Block(testAnswers); blocktest1.setButtons();
+                Block blocktest2 = new Block(testAnswers); blocktest2.setButtons();
+                Block blocktest3 = new Block(testAnswers); blocktest3.setButtons();
+		GridPane master = new GridPane();
+	        GridPane grid1 = blocktest1.returnGrid();
+                GridPane grid2 = blocktest2.returnGrid();
+                GridPane grid3 = blocktest3.returnGrid();
+
+		master.add(grid1,0,0);
+                master.add(grid2,0,1);
+                master.add(grid3,1,0);
+
+         
+	        Scene scene = new Scene(master);
 	        primaryStage.setScene(scene);
 	        primaryStage.show();
 
